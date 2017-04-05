f578ce3f039fda29af025a91c902674ce199ae5a
SAE\lwatson2 <liam_watson2003@hotmail.com>
2017-04-05 16:47:11 +1000

    Uploaded the initial flocking files

---
+++ Assets/Flocking.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flocking : MonoBehaviour {
+
+    public float speed;
+    public float rotatespeed;
+    public float speedadjustment;
+    private bool catchup;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * (catchup ? speedadjustment : 1));
+
+
+
+    }
+}
