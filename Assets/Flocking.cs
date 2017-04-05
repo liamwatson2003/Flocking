101b8e3db48c52d69f0bd211d8d865f41911aaee
SAE\lwatson2 <liam_watson2003@hotmail.com>
2017-04-05 17:54:37 +1000

    Added flocking speed adjustment, so far the speed of the objects corrects itslef and averages out between the two speeds of the objects

--- Assets/Flocking.cs
+++ Assets/Flocking.cs
@@ -5,23 +5,54 @@ using UnityEngine;
 public class Flocking : MonoBehaviour {
 
     public float speed;
-    public float rotatespeed;
-    public float speedadjustment;
+    public float maxSpeed = 1.2f;
+    public float minSpeed = 0.8f;
+    public float rotateSpeed;
+    public float speedAdjustment;
     private bool catchup;
+
+    public float searchRadius;
 
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        catchup = false;
+        speed = Random.Range(minSpeed, maxSpeed);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * (catchup ? speedadjustment : 1));
+        //move the object forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * (catchup ? speedAdjustment : 1));
 
+        //check for other objects within range
+        Collider[] ObjectsHit = Physics.OverlapSphere(transform.position, searchRadius);
+        if (ObjectsHit.Length > 1)
+        {
+            foreach (Collider CObject in ObjectsHit)
+            {
+                
+                    Debug.Log("collider working");
+                    //get the other objects speed
+                    float otherspeed = CObject.transform.GetComponent<Flocking>().speed;
 
-
+                    //average the speed out between th other objects
+                    if (speed < otherspeed && speed <= maxSpeed)
+                    {
+                        speed += speedAdjustment;
+                    }
+                    else if (speed > otherspeed && speed >= minSpeed)
+                    {
+                        speed -= speedAdjustment;
+                    }
+                    //adding break here increased performance massivly
+                    break;
+                
+            }
+        }
     }
+
+
 }
